ce027b52a43cbb5093c90860a49fa62e20b224c6
rama priyadi <rama.priyadi21@gmail.com>
2022-11-15 04:59:29 +0700

    r

--- app.py
+++ app.py
@@ -5,15 +5,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return "HAYYYYY"
 
-@app.route('/login', methods=['POST','GET'])
-def index():
-  if request.method == 'POST':
-    return render_template('index.html')
+# @app.route('/login', methods=['POST','GET'])
+# def index():
+#   if request.method == 'POST':
+#     return render_template('index.html')
   
-  else:
-    return render_template('index.html')
+#   else:
+#     return render_template('index.html')
 
-if __name__ =="__main__":
-  app.run(debug=True)
+# if __name__ =="__main__":
+#   app.run(debug=True)
